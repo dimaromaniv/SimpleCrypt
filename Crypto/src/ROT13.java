be7ecaffca39198c91492e70583538de928d26dc
Dmytro_Romaniv <128440171+dimaromaniv@users.noreply.github.com>
2023-07-29 21:01:03 -0400

    finished and did sonnet too.

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -1,10 +1,17 @@
+import java.io.*;
+
 import static java.lang.Character.isLowerCase;
 import static java.lang.Character.isUpperCase;
 import static java.lang.Character.toLowerCase;
 
-public class ROT13  {
+public class ROT13 {
+
+    Character cs;
+    Character cf;
 
     ROT13(Character cs, Character cf) {
+        this.cs = cs;
+        this.cf = cf;
     }
 
     ROT13() {
@@ -12,21 +19,118 @@ public class ROT13  {
 
 
     public String crypt(String text) throws UnsupportedOperationException {
+        StringBuilder cryptedText = new StringBuilder();
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        Character keyToCrypt = Character.toUpperCase(cf);
+        String cryptoRotate = rotate(alphabet, keyToCrypt);
 
-        return "";
+        for (int i = 0; i < text.length(); i++) {
+            Character charToCrypt = text.charAt(i);
+            if (!Character.isLetter(charToCrypt)) {
+                cryptedText.append(charToCrypt);
+            } else if (!Character.isUpperCase(charToCrypt)) {
+                Character temp = Character.toUpperCase(charToCrypt);
+                int alphabetIndex = alphabet.indexOf(temp);
+                int cryptoIndex = alphabetIndex;
+                cryptedText.append(Character.toLowerCase(cryptoRotate.charAt(cryptoIndex)));
+            } else {
+                Character temp = charToCrypt;
+                int alphabetIndex = alphabet.indexOf(temp);
+                int cryptoIndex = alphabetIndex;
+                cryptedText.append(cryptoRotate.charAt(cryptoIndex));
+            }
+        }
+        return cryptedText.toString();
     }
 
+
     public String encrypt(String text) {
-        return text;
+
+
+        return crypt(text);
     }
 
     public String decrypt(String text) {
-        return text;
+        return crypt(text);
     }
 
-    public static String rotate(String s, Character c) {
 
-        return "";
+    public static String rotate(String s, Character c) {
+        int index = s.indexOf(c);
+
+        StringBuilder newString = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            int currentIndex = (index + i) % s.length();
+            newString.append(s.charAt(currentIndex));
+        }
+        return newString.toString();
+    }
+
+
+    public void fileReaderAndCrypter() {
+        StringBuilder sonnet = new StringBuilder();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("/Users/dima/Projects/SimpleCrypt/sonnet18.txt"));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sonnet.append(line).append("\n");
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String[] sonnetArr = sonnet.toString().split("\n");
+        for (int i = 0; i < sonnetArr.length; i++) {
+            sonnetArr[i] = encrypt(sonnetArr[i]);
+        }
+
+        try {
+            FileWriter writer = new FileWriter("/Users/dima/Projects/SimpleCrypt/sonnetCrypto.txt");
+            for (String line : sonnetArr) {
+                writer.write(line + "\n");
+            }
+            writer.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String decryptedOfTHeFile() {
+
+        StringBuilder decryptText = new StringBuilder();
+        String line;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("/Users/dima/Projects/SimpleCrypt/sonnetCrypto.txt"));
+
+            while ((line = reader.readLine()) != null) {
+                decryptText.append(line).append("\n");
+            }
+            reader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String[] decryptArray = decryptText.toString().split("\n");
+
+        for (int i = 0; i < decryptArray.length; i++) {
+            decryptArray[i] = decrypt(decryptArray[i]);
+        }
+        StringBuilder finalDecrypt = new StringBuilder();
+
+        for (String lines : decryptArray) {
+            finalDecrypt.append(lines).append("\n");
+        }
+        return finalDecrypt.toString();
     }
 
 }
+
+
+
+
+
+
--- Crypto/src/ROT13Test.java
+++ Crypto/src/ROT13Test.java
@@ -1,6 +1,8 @@
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ROT13Test {
 
@@ -14,6 +16,7 @@ public class ROT13Test {
         // When
         ROT13 cipher = new ROT13();
         String actual = cipher.rotate(s1, 'A');
+        System.out.println(actual.toString());
 
         // Then
         assertTrue(actual.equals(s2));
@@ -28,7 +31,7 @@ public class ROT13Test {
         // When
         ROT13 cipher = new ROT13();
         String actual = cipher.rotate(s1, 'D');
-
+        System.out.println(actual.toString());
         // Then
         assertTrue(actual.equals(s2));
     }
@@ -44,6 +47,7 @@ public class ROT13Test {
         String actual = cipher.rotate(s1, 'N');
         System.out.println(s1);
         System.out.println(actual);
+        System.out.println(actual.toString());
         // Then
         assertTrue(actual.equals(s2));
     }
@@ -86,6 +90,54 @@ public class ROT13Test {
         System.out.println(actual);
         // Then
         assertTrue(actual.equals(Q1));
+    }
+
+//    @Test
+//    public void testSonnet () {
+//
+//        ROT13 cipher = new ROT13();
+//
+//        String expected = "Shall I compare thee to a summer’s day?\n" +
+//                "Thou art more lovely and more temperate:\n" +
+//                "Rough winds do shake the darling buds of May,\n" +
+//                "And summer’s lease hath all too short a date;\n" +
+//                "Sometime too hot the eye of heaven shines,\n" +
+//                "And often is his gold complexion dimm'd;\n" +
+//                "And every fair from fair sometime declines,\n" +
+//                "By chance or nature’s changing course untrimm'd;\n" +
+//                "But thy eternal summer shall not fade,\n" +
+//                "Nor lose possession of that fair thou ow’st;\n" +
+//                "Nor shall death brag thou wander’st in his shade,\n" +
+//                "When in eternal lines to time thou grow’st:\n" +
+//                "   So long as men can breathe or eyes can see,\n" +
+//                "   So long lives this, and this gives life to thee." ;
+//        //String st = cipher.fileReader();
+//       // String actual = cipher.crypt(st);
+//       // Assert.assertEquals(expected,actual);
+//    }
+
+
+    @Test
+    public void sonnetTest() {
+        ROT13 cipher = new ROT13('a', 'n');
+        String expected = "Shall I compare thee to a summer’s day?\n" +
+                "Thou art more lovely and more temperate:\n" +
+                "Rough winds do shake the darling buds of May,\n" +
+                "And summer’s lease hath all too short a date;\n" +
+                "Sometime too hot the eye of heaven shines,\n" +
+                "And often is his gold complexion dimm'd;\n" +
+                "And every fair from fair sometime declines,\n" +
+                "By chance or nature’s changing course untrimm'd;\n" +
+                "But thy eternal summer shall not fade,\n" +
+                "Nor lose possession of that fair thou ow’st;\n" +
+                "Nor shall death brag thou wander’st in his shade,\n" +
+                "When in eternal lines to time thou grow’st:\n" +
+                "   So long as men can breathe or eyes can see,\n" +
+                "   So long lives this, and this gives life to thee.\n";
+
+        cipher.fileReaderAndCrypter();
+        String actual = cipher.decryptedOfTHeFile();
+        Assert.assertEquals(expected, actual);
     }
 
 }
